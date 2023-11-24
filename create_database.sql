60878bcbb736d9bf7c6733172e37e657ba2e3604
nabilettihadi <nabilettihadi.ma@gmail.com>
2023-11-24 12:06:06 +0100

    DataWare - Gestion des Ressources Humaines

--- create_database.sql
+++ create_database.sql
@@ -8,7 +8,7 @@ CREATE TABLE Equipe (
 );
 
 CREATE TABLE Membre (
-    IDMembre INT PRIMARY KEY,
+    IDMembre INT AUTO_INCREMENT PRIMARY KEY,
     Nom VARCHAR(255),
     Prenom VARCHAR(255),
     Email VARCHAR(255),
@@ -21,10 +21,14 @@ CREATE TABLE Membre (
 
 
 INSERT INTO Equipe VALUES (1, 'Equipe A', '2023-01-01');
-INSERT INTO Equipe VALUES (2, 'Equipe A', '2023-09-01');
-INSERT INTO Equipe VALUES (1, 'Equipe B', '2023-08-01');
-INSERT INTO Equipe VALUES (1, 'Equipe B', '2023-11-01');
-INSERT INTO Membre VALUES (1, 'Nom1', 'Prenom1', 'email1@example.com', '123456789', 'Role1', 1, 'Actif');
-INSERT INTO Membre VALUES (1, 'Nom2', 'Prenom2', 'email1@example.com', '3456789', 'Role2', 2, '');
-INSERT INTO Membre VALUES (1, 'Nom3', 'Prenom3', 'email1@example.com', '12456789', 'Role1', 1, 'Actif');
-INSERT INTO Membre VALUES (1, 'Nom3', 'Prenom4', 'email1@example.com', '1236789', 'Role2', 2, '');
+INSERT INTO Equipe VALUES (2, 'Equipe B', '2023-09-01');
+INSERT INTO Equipe VALUES (3, 'Equipe C', '2023-10-01');
+
+INSERT INTO Membre VALUES (1, 'Ettihadi', 'Nabil', 'nabil@gmail.com', '0612345678', 'Designer', 1, 'Actif');
+INSERT INTO Membre VALUES (2, 'hatimi', 'Amine', 'amine@gmail.com', '0610203040', 'Developer', 2, 'Inactif');
+INSERT INTO Membre VALUES (3, 'berrada', 'Yassir', 'yassir@gmail.com', '0610020030', 'Designer', 1, 'Actif');
+INSERT INTO Membre VALUES (4, 'baraka', 'Kamal', 'kamal@gmail.com', '0620050080', 'Developer', 3, 'Actif');
+INSERT INTO Membre VALUES (5, 'saadan', 'Mohammed', 'mohamed@gmail.com', '0670020030', 'Designer', 1, 'Inactif');
+INSERT INTO Membre VALUES (6, 'kotaibi', 'Rayan', 'rayan@gmail.com', '0625507500', 'Developer', 2, 'Inactif');
+INSERT INTO Membre VALUES (7, 'hachimi', 'Salim', 'salim@gmail.com', '0615304560', 'Designer', 2, 'Actif');
+INSERT INTO Membre VALUES (8, 'Rami', 'Badr', 'badr@gmail.com', '0620406080', 'Developer', 3, 'Inactif');
--- display_data.php
+++ display_data.php
@@ -1,31 +1,88 @@
 <?php
 include 'connect_database.php';
 
-$sql = "SELECT * FROM Membre";
-$result = mysqli_query($conn, $sql);
+// Fetch members
+$memberSql = "SELECT * FROM Membre";
+$memberResult = mysqli_query($conn, $memberSql);
 
-if (mysqli_num_rows($result) > 0) {
-    echo "<table border='1'>";
-    echo "<tr><th>ID Membre</th><th>Nom</th><th>Prénom</th><th>Email</th><th>Téléphone</th><th>Rôle</th><th>ID Équipe</th><th>Statut</th></tr>";
+// Fetch teams
+$teamSql = "SELECT * FROM Equipe";
+$teamResult = mysqli_query($conn, $teamSql);
 
-    while ($row = mysqli_fetch_assoc($result)) {
+echo "<div class='mb-8'>";
+// Display member table
+if (mysqli_num_rows($memberResult) > 0) {
+    echo "<h2 class='text-2xl font-bold mb-4 text-center'>Tableau des Membres</h2>";
+    echo "<div class='overflow-x-auto'>";
+    echo "<table class='table-auto w-full'>";
+    echo "<thead>";
+    echo "<tr>";
+    echo "<th class='px-4 py-2 border'>ID Membre</th>";
+    echo "<th class='px-4 py-2 border'>Nom</th>";
+    echo "<th class='px-4 py-2 border'>Prénom</th>";
+    echo "<th class='px-4 py-2 border'>Email</th>";
+    echo "<th class='px-4 py-2 border'>Téléphone</th>";
+    echo "<th class='px-4 py-2 border'>Rôle</th>";
+    echo "<th class='px-4 py-2 border'>ID Équipe</th>";
+    echo "<th class='px-4 py-2 border'>Statut</th>";
+    echo "</tr>";
+    echo "</thead>";
+    echo "<tbody>";
+
+    while ($row = mysqli_fetch_assoc($memberResult)) {
         echo "<tr>";
-        echo "<td>" . $row["IDMembre"] . "</td>";
-        echo "<td>" . $row["Nom"] . "</td>";
-        echo "<td>" . $row["Prenom"] . "</td>";
-        echo "<td>" . $row["Email"] . "</td>";
-        echo "<td>" . $row["Telephone"] . "</td>";
-        echo "<td>" . $row["Role"] . "</td>";
-        echo "<td>" . $row["IDEquipe"] . "</td>";
-        echo "<td>" . $row["Statut"] . "</td>";
+        echo "<td class='px-4 py-2 border'>" . $row["IDMembre"] . "</td>";
+        echo "<td class='px-4 py-2 border'>" . $row["Nom"] . "</td>";
+        echo "<td class='px-4 py-2 border'>" . $row["Prenom"] . "</td>";
+        echo "<td class='px-4 py-2 border'>" . $row["Email"] . "</td>";
+        echo "<td class='px-4 py-2 border'>" . $row["Telephone"] . "</td>";
+        echo "<td class='px-4 py-2 border'>" . $row["Role"] . "</td>";
+        echo "<td class='px-4 py-2 border'>" . $row["IDEquipe"] . "</td>";
+        echo "<td class='px-4 py-2 border'>" . $row["Statut"] . "</td>";
         echo "</tr>";
-        // Vous pouvez ajouter d'autres colonnes ici...
     }
 
+    echo "</tbody>";
     echo "</table>";
+    echo "</div>";
 } else {
-    echo "0 results";
+    echo "<p class='text-center'>0 results for Members</p>";
 }
+
+echo "</div>";
+
+echo "<div class='mb-8'>";
+// Display team table
+if (mysqli_num_rows($teamResult) > 0) {
+    echo "<h2 class='text-2xl font-bold mb-4 text-center'>Tableau des Équipes</h2>";
+    echo "<div class='overflow-x-auto'>";
+    echo "<table class='table-auto w-full'>";
+    echo "<thead>";
+    echo "<tr>";
+    echo "<th class='px-4 py-2 border'>ID Équipe</th>";
+    echo "<th class='px-4 py-2 border'>Nom Équipe</th>";
+    echo "<th class='px-4 py-2 border'>Date de Création</th>";
+    echo "</tr>";
+    echo "</thead>";
+    echo "<tbody>";
+
+    while ($row = mysqli_fetch_assoc($teamResult)) {
+        echo "<tr>";
+        echo "<td class='px-4 py-2 border'>" . $row["IDEquipe"] . "</td>";
+        echo "<td class='px-4 py-2 border'>" . $row["NomEquipe"] . "</td>";
+        echo "<td class='px-4 py-2 border'>" . $row["DateCreation"] . "</td>";
+        echo "</tr>";
+    }
+
+    echo "</tbody>";
+    echo "</table>";
+    echo "</div>";
+} else {
+    echo "<p class='text-center'>0 results for Teams</p>";
+}
+
+echo "</div>";
 
 mysqli_close($conn);
 ?>
+
